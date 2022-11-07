623c003f0ed2f7a2dc2ac5a57dc9a79be804465a
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2022-11-07 22:44:35 +0300

    Created factory design pattern

---
+++ src/factory/App.java
@@ -0,0 +1,11 @@
+package factory;
+
+public class App {
+	public static void main(String[] args) {
+		Computer pc = Factory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+		Computer server = Factory.getComputer("server","16 GB","1 TB","2.9 GHz");
+		
+		System.out.println("Factory PC Config::"+pc);
+		System.out.println("Factory Server Config::"+server);
+	}
+}
