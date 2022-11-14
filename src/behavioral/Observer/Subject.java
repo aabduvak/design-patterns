a50fa5557d71cb8752d34abf1baac563937c9f2c
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2022-11-14 19:17:33 +0300

    Created Observer Pattern

---
+++ src/behavioral/Observer/Subject.java
@@ -0,0 +1,13 @@
+package behavioral.Observer;
+
+public interface Subject {
+	//methods to register and unregister observers
+	public void register(Observer observer);
+	public void unregister(Observer observer);
+	
+	//method to notify observers of change
+	public void notifyObservers();
+	
+	//method to get updates from subject
+	public Object getUpdate(Observer observer);
+}
---
+++ src/behavioral/Observer/Subscriber.java
@@ -0,0 +1,25 @@
+package behavioral.Observer;
+
+public class Subscriber implements Observer{
+
+	private String name;
+	private Subject topic;
+	
+	public Subscriber (String name){
+		this.name = name;
+	}
+	
+	@Override
+	public void update() {
+		String msg = (String) topic.getUpdate(this);
+		if(msg == null){
+			System.out.println(name+":: No new message");
+		}else
+			System.out.println(name+":: Consuming message::"+msg);
+	}
+
+	@Override
+	public void setSubject(Subject sub) {
+		this.topic=sub;
+	}
+}
